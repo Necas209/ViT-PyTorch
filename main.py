90c207ece325951029d0fa83f451945ba538ad5c
Diogo Medeiros <diogo_medeiros@sapo.pt>
2022-05-20 13:43:04 +0100

    ViT 1.1.0

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ import argparse
 import os
 import shutil
 
-import numpy as np
-import scipy.io as sio
 import timm
 from tensorboard_logger import configure
 from torch import optim
 from torch.backends import cudnn
-from torch.utils import data
-from torch.utils.data import TensorDataset
 from torchsummary import summary
 
 from train import *
@@ -45,46 +41,8 @@ def main():
     args = parser.parse_args()
     if args.tensorboard:
         configure(f"runs/{args.name}")
-    # Data loading
-    if args.data is False:
-        mat = sio.loadmat('Hyper_2012_TF_sugar.mat')
-        x: np.ndarray = mat['MeanROffPedNorm']
-        y: np.ndarray = mat['sugar']
-        x = x.transpose()[:, 10:-6].reshape(240, 1, 32, 32)
-        t_x = torch.Tensor(x)
-        t_y = torch.Tensor(y)
-        # Shuffle tensors' rows
-        indices = torch.randperm(t_x.size()[0])
-        t_x = t_x[indices]
-        t_y = t_y[indices]
-        # Split dataset into train, validation and test sets
-        dataset = TensorDataset(t_x, t_y)
-        ds_size = len(dataset)
-        test_size = int(0.1 * ds_size)
-        train_size = ds_size - test_size
-        ds, test_ds = data.random_split(dataset, [train_size, test_size])
-        ds_size = len(ds)
-        val_size = int(0.1 * ds_size)
-        train_size = ds_size - val_size
-        train_ds, val_ds = data.random_split(ds, [train_size, val_size])
-        # Create data loaders
-        kwargs = {'num_workers': 4, 'pin_memory': True}
-        train_loader = data.DataLoader(train_ds, batch_size=args.batch_size, shuffle=True, **kwargs)
-        val_loader = data.DataLoader(val_ds, batch_size=args.batch_size, shuffle=True, **kwargs)
-        test_loader = data.DataLoader(test_ds, batch_size=args.batch_size, shuffle=True, **kwargs)
-        # Save data loaders to file
-        if args.resume is False:
-            d = {'train_loader': train_loader,
-                 'val_loader': val_loader,
-                 'test_loader': test_loader}
-            torch.save(d, f'./runs/{args.name}/dataloader_dict.pt')
-    else:
-        dl_path = f'./runs/{args.name}/dataloader_dict.pt'
-        print(f'Loading data from {dl_path}')
-        d = torch.load(dl_path)
-        train_loader = d['train_loader']
-        val_loader = d['val_loader']
-        test_loader = d['test_loader']
+    # Load data
+    train_loader, val_loader, test_loader = load_data(args)
     # Model creation
     model = timm.create_model(args.model,
                               pretrained=True,
